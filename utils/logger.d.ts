002de91cecf34ad94b64edcfe73625544be745f0
dhyabi2 <32069256+dhyabi2@users.noreply.github.com>
2025-06-12 23:33:47 +0400

    clear

--- utils/logger.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Logger {
-    private logDir;
-    constructor(logDir: string);
-    private ensureLogDirectory;
-    private getTimestamp;
-    private writeLog;
-    log(type: string, data?: any): void;
-    logError(type: string, error: Error | any): void;
-}
